536c6c351075a6b8ad12bbba195b365abb5dcee6
Primero1800 <Primero@inbox.ru>
2025-06-04 11:18:28 +0300

    feat(app) error_base tool added

---
+++ src/tools/errors_base.py
@@ -0,0 +1,35 @@
+class ErrorsBase:
+    CLASS = "Object"
+    _CLASS = "object"
+
+    @classmethod
+    def HANDLER_MESSAGE(cls):
+        return f"Handled by {cls.CLASS}s exception handler"
+
+    @classmethod
+    def DATABASE_ERROR(cls):
+        return "Error occurred while changing database data"
+
+    @classmethod
+    def NOT_EXISTS(cls):
+        return f"{cls.CLASS} doesn't exist"
+
+    @classmethod
+    def ALREADY_EXISTS(cls):
+        return f"{cls.CLASS} already exists"
+
+    @classmethod
+    def NO_RIGHTS(cls):
+        return "You are not authorized for this operation"
+
+    @staticmethod
+    def INVALID_PASSWORD():
+        return "Invalid password"
+
+    @staticmethod
+    def INVALID_TOKEN():
+        return "Invalid token"
+
+    @staticmethod
+    def BAD_CREDENTIALS_OR_NOT_ACTIVE():
+        return "Bad credentials or user is not active"
