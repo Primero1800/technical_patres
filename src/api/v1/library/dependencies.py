3e2eb08009b2c9df77d55a119bafe1eafa850688
Primero1800 <Primero@inbox.ru>
2025-06-05 10:59:10 +0300

    refactoring(app) Library dependencies edited, get_one added

--- src/api/v1/library/dependencies.py
+++ src/api/v1/library/dependencies.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import DBConfigurer
+from .service import LibraryService
 from ..readers.dependencies import get_one_complex_actual as reader_get_one_complex_actual
 from ..books.dependencies import get_one as book_get_one
 
@@ -11,7 +12,18 @@ if TYPE_CHECKING:
     from src.core.models import (
         Reader,
         Book,
+        BorrowedBook,
     )
+
+
+async def get_one(
+        borrow_id: int,
+        session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> "BorrowedBook":
+    service: LibraryService = LibraryService(
+        session=session
+    )
+    return await service.get_one(borrow_id)
 
 
 async def get_reader_complex_actual(
