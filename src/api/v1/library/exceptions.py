1aa2445bf1b5f5747576fee330e038a941c1c47f
Primero1800 <Primero@inbox.ru>
2025-06-05 10:58:30 +0300

    refactoring(app) Library exceptions edited

--- src/api/v1/library/exceptions.py
+++ src/api/v1/library/exceptions.py
@@ -2,12 +2,20 @@ from src.tools.errors_base import ErrorsBase
 
 
 class Errors(ErrorsBase):
-    CLASS = "Library"
-    _CLASS = "library"
+    CLASS = "BorrowedBook"
+    _CLASS = "borrowed_book"
+
+    @classmethod
+    def HANDLER_MESSAGE(cls):
+        return f"Handled by Library exception handler"
 
     @staticmethod
     def NOT_ENOUGH_QUANTITY():
         return "Not enough quantity"
+
+    @classmethod
+    def INVALID_OPERATION(cls):
+        return f"Invalid operation. The item has already been returned"
 
     @staticmethod
     def LIMIT_REACHED():
